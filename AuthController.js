39b1854dc25ffaf7cd0263ebe4aed75a552b4f7b
ckopecky <ckopecky@github.com>
2018-11-15 13:26:51 -0800

    changed AuthController

--- AuthController.js
+++ AuthController.js
@@ -1,70 +1,93 @@
+const User = require('./authSchema');
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
-const Auths = require("./AuthSchema");
-require("dotenv")
-const tokenGenerator = auth => {
+const mysecret = "You're a wizard, Harry";
+
+const generateToken = (user) => {
     const options = {
-        expiresIn: "24h"
+        expiresIn: "1h",
     };
-
     const payload = {
-        name: auth.username
-    };
-    const secret = "You're a wizard, Harry.";
+        username: user.username
+    }; // what will determine our payload.
+    return jwt.sign(payload, mysecret, options); // creates our JWT with a secret and a payload and a hash.
+}
 
-
-
-    return jwt.sign(payload, secret, options);
-};
-
-const login = (req, res) => {
-    const { username, password } = req.body;
-    Auths.findOne({username})
-        .then(auth => {
-            if(!auth) {
-                res.status(404).json({ Error: "User not found"});
+    const register = (req, res) => {
+        // create user takes in the username and password and saves a user.
+        // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+        const { username, password, confirmPassword, firstName, lastName, email } = req.body;
+        User
+            .create({ username, password, firstName, lastName, email})
+            .then(user => {
+            if(user.username){
+                const token = generateToken(user);
+                res.status(201).json({user: user.username, headers: { Authentication: token }});
             }
-            else {
-                console.log(auth);
-                auth
-                    .validatePassword(password)
-                    .then(match => {
-                        if(match) {
-                            const token = tokenGenerator(auth);
-                            res.status(200).json({ Message: `Welcome, ${auth.firstName}, have a token`, token})
-                        }
-                        else {
-                            res.status(400).json({ Error: "username/password combination not found" });
-                        }
-                    })
-                    .catch(err => {
-                        res.status(500).json({ Error: err.message });
-                    });
+            else{
+                res.status(422).json({Error:err.message})
             }
-        });
-};
-
-const register = (req, res) => {
-    const { username, password, confirmedPassword, firstName, lastName } = req.body;
-    if(password !== confirmedPassword) {
-        res.status(400).json( { Error: "passwords do not match!" });
-    }
-    else if(!username, !password, !confirmedPassword, !firstName, !lastName) {
-        res.status(400).json( { Error: "must include username, password, confirmedPassword, firstName, lastName"});
-    }
-    else {
-        Auths.create(req.body)
-            .then(auth => {
-                const token = tokenGenerator(auth);
-                res.status(201).json({username: auth.username, firstName: auth.firstName, lastName: auth.lastName, password: auth.password,headers: { Authentication: token }});
             })
             .catch(err => {
-                res.status(500).json({ "Error from register catch" : err.message });
+            res.status(500).json({Error: err.message});
             });
-    }
-};
+        };
 
-router.post("/login", login);
-router.post("/register", register);
+    const login = (req, res) => {
+        const { username, password } = req.body;
+        User.findOne({ username }, (err, user) => {
+            if (err) {
+                res.status(403).json({ error: 'Invalid Username/Password' });
+                return;
+            }
+            else if (user === null || user === undefined) {
+                res.status(422).json({ error: 'No user with that username in our DB' });
+                return;
+            } else{
+                user.validatePassword(password)
+                // This is an example of using our User.method from our model.
+                .then(hashMatch => {
+                if (hashMatch) {
+                    const token = generateToken(user);
+                    res.status(200).json({ message: `Hello, ${user.username}`,token }); 
+                    return;
+                    // sends the token back to the client
+                }
+                else {
+                    res.status(422).json({ error: 'Invalid password' });
+                    return;
+                }
+                })
+                .catch(err => {
+                    res.status(500).json({Error: err.message});
+                });
+            }
+        });
+    };
 
-module.exports = router;
+    const get = (req, res) => { //works
+        User.find()
+            .then(users => {
+                console.log(users);
+                if(users.length===0){
+                    res.status(404).json({Message: "users not found"})
+                } else {
+                    res.status(200).json(users);
+                }
+            
+            })
+            .catch(err => {
+                res.status(500).json({Error: "There was an error in retrieving users", err});
+            });
+    
+    };
+    router.route("/register")
+        .post(register);
+
+    router.route("/login")
+        .post(login);
+
+    router.route("/users")
+        .get(get);
+
+    module.exports = router;
